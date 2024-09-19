5af54875802384c1c2e4bca44115605b41dfe985
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-09-19 17:38:18 -0400

    Instanciando objetos de prueba de la clase saiyajin

--- saiyajin.php
+++ saiyajin.php
@@ -1,2 +1,21 @@
 <?php
-    echo "hello world";
+    class Saiyajin{
+        public string $nombre = "Goku";
+        public int $nivel_pelea = 1000;
+
+        public function Saludar():string{
+            return "Hola soy ".$this->nombre;
+        }
+
+        public function NivelDePelea(){
+            return "$this->nombre tiene un nivel de pelea de $this->nivel_pelea";
+        }
+    }
+
+    $goku = new Saiyajin();
+
+    echo $goku->nombre."<br />";
+    echo $goku->NivelDePelea()."<br />";
+
+    $vegeta = new Saiyajin();
+    var_dump($vegeta);
